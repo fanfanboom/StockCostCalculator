ad4f7c155e71947de490f4dd08a3fbea13e3c26e
fanfanboom <zhangyunfan716@gmail.com>
2022-02-23 11:01:38 +0800

    股票成本计算器，js版本

---
+++ 股票成本计算器.js
@@ -0,0 +1,27 @@
+function stockcostcalculator() {
+    let inpcontroll=true;
+    let prices=[];
+    let numbers=[];
+    let p='';
+    let n='';
+    while (inpcontroll){
+        p=prompt('请输入价格（空表示输入结束）');
+        if (p === ''){
+            inpcontroll=false;
+        }else{
+            n=prompt('请输入交易数量，如100');
+            prices.push(Number.parseFloat(p));
+            numbers.push(Number.parseInt(n));
+        }
+    }
+    let infotext='共交易'+prices.length.toString()+'次<br>';
+    let sumcost=0;
+    let sumnumbers=0;
+    for (let i = 0; i < prices.length; i++) {
+        sumcost+=prices[i]*numbers[i];
+        sumnumbers+=numbers[i];
+        infotext=infotext+i.toString()+'.成交价：'+prices[i].toString()+', 成交数量：'+numbers[i].toString()+'.<br>';
+    }
+    let ave=sumcost/sumnumbers;
+    return {ave,sumcost,sumnumbers,infotext};
+}
